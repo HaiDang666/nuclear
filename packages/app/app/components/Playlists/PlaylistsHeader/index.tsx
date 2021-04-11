354a783fc01a45ff20eba63b2c180de4ef9c332e
Felix Hoang <hdcompany123@gmail.com>
2021-04-11 17:09:20 +0700

    wip: import modal

--- packages/app/app/components/Playlists/PlaylistsHeader/index.tsx
+++ packages/app/app/components/Playlists/PlaylistsHeader/index.tsx
@@ -1,18 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Button, Modal, Input, Dropdown } from 'semantic-ui-react';
 
+import Header from '../../Header';
 import styles from './styles.scss';
 
 type PlaylistsHeaderProps = {
   showText: boolean;
 }
 
-const PlaylistsHeader: React.FC<PlaylistsHeaderProps> = ({showText}) => {
+const friendOptions = [
+  {
+    key: 'Youtube',
+    text: 'Youtube',
+    value: 'Youtube'
+  },
+  {
+    key: 'Playlist',
+    text: 'Playlist',
+    value: 'Playlist'
+  }
+];
+
+const PlaylistsHeader: React.FC<PlaylistsHeaderProps> = ({ showText }) => {
   const { t } = useTranslation('playlists');
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className={styles.header_container}>
-      {showText && t('header')}
+      {showText && <Header>{t('header')}</Header>}
+      {!showText && <span />}
+
+      <Modal
+        trigger={<Button basic icon={'cloud download'} />}
+        on='click'
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        onOpen={() => setIsOpen(true)}
+        className={styles.import_modal}
+      >
+        <Modal.Content>
+          <section className={styles.source_section}>
+            <Header>{t('import')}</Header>
+            <hr />
+            <div className={styles.playlist_url}>
+              <label>URL</label>
+              <Input placeholder='URL...' />
+            </div>
+
+            <div>
+              <label>
+                {t('select-source')}
+              </label>
+              <Dropdown
+                selection
+                options={friendOptions}
+                defaultValue={friendOptions[0].value}
+                onChange={() => { }}
+              />
+            </div>
+          </section>
+          <Button
+            color='black'
+            content='Import'
+            labelPosition='left'
+            icon='cloud download'
+            onClick={() => setIsOpen(false)}
+            positive
+          />
+        </Modal.Content>
+      </Modal>
     </div>
   );
 };
